dbac0e023a8bf53b9208924d431f63bdf784d784
MaikenPR <438552@rts-365.dk>
2018-08-29 14:30:04 +0200

    avanceret søgning virker

--- routes/search.js
+++ routes/search.js
@@ -14,7 +14,9 @@ module.exports = function (app) {
     app.post('/search', async (req, res) => {
         // console.log('body: ', req.body)
         let txt = req.body.txt;
-        let produkter = await produkt.search(txt);
+        let kategori = req.body.kategori;
+        let pris = req.body.pris;
+        let produkter = await produkt.search(txt, kategori, pris);
         const kategorier = await Produkter.getKategorier();
         console.log('route produkter: ', produkter);
         console.log(kategorier);
--- services/search.js
+++ services/search.js
@@ -2,21 +2,29 @@ const db = require('../config/database').connect();
 
 module.exports = {
 
-    search: function (txt) {
+    search: function (txt, kategori = [], pris) {
         txt = txt == '' ? '_' : txt;
         
-        console.log('service: ', 'txt=', txt)
+        if (kategori.length == 0) {
+            for (i = 0; i < 10; i++)kategori.push(i);
+        }
+        pris = (pris == '' || isNaN(pris))? 1000000 : pris;
+
+        console.log('service: ', 'txt=', txt, 'kategori=', kategori, 'pris=', pris)
         return new Promise((resolve, reject) => {
-            let prepare = ['%' + txt + '%']
+            let prepare = ['%' + txt + '%', kategori, pris]
             var sql = `
                 SELECT
                     produkt.id,
                     produkt.navn,
                     produkt.billede,
+                    produkt.pris,
                     kategori.navn AS kategori
                 FROM produkt
                 INNER JOIN kategori ON fk_kategori = kategori.id
-                WHERE produkt.navn like ?
+                WHERE produkt.navn like ? and
+                produkt.fk_kategori in (?) and
+                produkt.pris <= ?
             `;
             db.query(sql, prepare, function (err, result) {
                 if (err) reject('fejl');
